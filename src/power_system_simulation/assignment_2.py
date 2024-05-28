efe45ed32f44d9317a025e5c88875b00a0c0add2
joshua-d-palomino-luna <167432588+joshua-d-palomino-luna@users.noreply.github.com>
2024-05-28 10:29:23 +0200

    file for ass.2, test file as well, many chnges nec

--- src/power_system_simulation/assignment_2.py
+++ src/power_system_simulation/assignment_2.py
@@ -1,1 +1,38 @@
-# file to write code for assignment 2
+"""
+This file contains code required for assignment 2.
+
+We define a class that deals with power grid models and load profiles, with some functions required for assignment 2.
+"""
+
+import numpy as np
+import pandas as pd
+from power_grid_model import CalculationMethod, CalculationType, LoadGenType, PowerGridModel, initialize_array
+
+
+class ProfileTimestampsDoesNotMatchError(Exception):
+    pass
+
+
+class ProfileLoadIDSDoesNotMatchError(Exception):
+    pass
+
+
+# add exception 1 here
+
+# add exception 2 here
+
+
+class PowerGridCalculation:
+    """
+    add documentation here
+    """
+
+    def __init__(
+        self,
+        power_grid: PowerGridModel,
+        active_load_profile: List[float],
+        reactive_load_profile: List[float],
+    ) -> None:
+        """
+        add documentation
+        """

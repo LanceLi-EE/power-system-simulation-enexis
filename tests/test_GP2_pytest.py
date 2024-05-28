68bf3eccb005acfc826ddf677c4b5a4b0f52953a
Lennartwolf <167818588+Lennartwolf@users.noreply.github.com>
2024-05-28 17:20:30 +0200

    Update test_GP2_pytest.py

--- tests/test_GP2_pytest.py
+++ tests/test_GP2_pytest.py
@@ -1,13 +1,13 @@
 import pytest
 from power_system_simulation.graph_processing import (
-    GraphProcessor, 
+    GraphProcessor,
     IDNotUniqueError,
     InputLengthDoesNotMatchError,
     IDNotFoundError,
     GraphNotFullyConnectedError,
     GraphCycleError,
-    EdgeAlreadyDisabledError
-    )
+    EdgeAlreadyDisabledError,
+)
 
 
 def test_ini_case1():
@@ -20,7 +20,7 @@ def test_ini_case1():
     with pytest.raises(IDNotUniqueError):
         # Attempt to create an instance of GraphProcessor
         gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
 
 def test_ini_case2():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -81,6 +81,7 @@ def test_ini_case6():
         # Attempt to create an instance of GraphProcessor
         gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
 
+
 def test_ini_case7():
     vertex_ids = ["A", "B", "C", "D", "E"]
     edge_vertex_id_pairs = [("A", "B"), ("B", "C"), ("C", "D"), ("C", "E"), ("B", "E")]
@@ -100,10 +101,10 @@ def test_downstream_case1():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
     with pytest.raises(IDNotFoundError):
         print(gp.find_downstream_vertices(6))
-    
+
 
 def test_downstream_case2():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -112,10 +113,10 @@ def test_downstream_case2():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
     # No error is being checked here
     print("downstream_case2() return:", gp.find_downstream_vertices(5))
-    
+
 
 def test_downstream_case3():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -124,10 +125,10 @@ def test_downstream_case3():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
     # No error is being checked here
     print("downstream_case3() return:", gp.find_downstream_vertices(3))
-    
+
 
 def test_alternative_case1():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -136,10 +137,10 @@ def test_alternative_case1():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
     with pytest.raises(IDNotFoundError) as excinfo:
         print("alternative_case1() return:", gp.find_alternative_edges(6))
-    
+
 
 def test_alternative_case2():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -148,10 +149,10 @@ def test_alternative_case2():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
     with pytest.raises(EdgeAlreadyDisabledError) as excinfo:
         print("alternative_case2() return:", gp.find_alternative_edges(5))
-    
+
 
 def test_alternative_case3():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -160,10 +161,10 @@ def test_alternative_case3():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
+
     # No error is being checked here
     print("alternative_case3() return:", gp.find_alternative_edges(1))
-    
+
 
 def test_alternative_case4():
     vertex_ids = ["A", "B", "C", "D", "E"]
@@ -172,11 +173,9 @@ def test_alternative_case4():
     edge_enabled = [1, 1, 1, 1, 0]
     source_vertex_id = "A"
     gp = GraphProcessor(vertex_ids, edge_ids, edge_vertex_id_pairs, edge_enabled, source_vertex_id)
-    
-    #No error is being checked here
+
+    # No error is being checked here
     print("alternative_case4() return:", gp.find_alternative_edges(2))
-    
+
+
 # add new cases here with the same structure
-
-
-

f0e17989f97b3dc11967c98b323266e4b2aef144
joshua-d-palomino-luna <167432588+joshua-d-palomino-luna@users.noreply.github.com>
2024-06-14 09:37:04 +0200

    optimized module for faster calc

--- src/power_system_simulation/power_system_simulation.py
+++ src/power_system_simulation/power_system_simulation.py
@@ -218,7 +218,7 @@ class ev_penetration_level:
 
 class optimal_tap_position:
     """
-    The class used to find optimmal tap position of the tramsformer accroding to the input criteria
+    The class used to find optimmal tap position of the tramsformer according to the input criteria
     """
 
     def __init__(self, low_voltage_network_data: str, active_load_profile: str, reactive_load_profile: str):
@@ -252,55 +252,70 @@ class optimal_tap_position:
         line_losses = []
         voltage_deviations = []
 
-        for tap_pos in tap_positions:
-            # update tap position in power grid input data
-            self.low_voltage_grid["transformer"]["tap_pos"] = [tap_pos]
-
-            # create power grid model with updated tap position
-            pow_grid_model = PowerGridModel(input_data=self.low_voltage_grid)
-
-            # run time series power flow calculation
-            pow_flow_result = pow_grid_model.calculate_power_flow(
-                update_data=self.load_profile_batch, calculation_method=CalculationMethod.newton_raphson
-            )
-
-            # extract relevant information about lines:
-
-            # line losses
-            p_loss = pd.DataFrame()
-            p_loss = pd.DataFrame(pow_flow_result["line"]["p_from"]) + pd.DataFrame(pow_flow_result["line"]["p_to"])
-
-            table_line_losses = pd.DataFrame()
-            table_line_losses["energy_loss_kw"] = 0.0
-
-            for (column_name, column_data), i in zip(p_loss.items(), enumerate(p_loss.columns)):
-                table_line_losses.loc[i[0], "energy_loss_kw"] = integrate.trapezoid(column_data.to_list()) / 1000
-
-            line_losses.append(sum(table_line_losses["energy_loss_kw"]))
-
-            # voltage deviations
-            table_voltages = pd.DataFrame()
-            table_voltages["max_pu"] = 0.0
-            table_voltages["min_pu"] = 0.0
-            i = 0
-            for node_scenario in pow_flow_result["node"]:
-                df_temp = pd.DataFrame(node_scenario)
-                max_value_pu = df_temp.at[df_temp["u_pu"].idxmax(), "u_pu"]
-                min_value_pu = df_temp.at[df_temp["u_pu"].idxmin(), "u_pu"]
-                table_voltages.loc[i, "max_pu"] = max_value_pu
-                table_voltages.loc[i, "min_pu"] = min_value_pu
-                i = i + 1
-
-            max_volt_deviation = max(max(abs(table_voltages["max_pu"] - 1)), max(abs(table_voltages["min_pu"] - 1)))
-            voltage_deviations.append(max_volt_deviation)
-
-        # find optimal tap bosition based on criteria chosen by user
         if optimization_criteria == "minimize_line_losses":
+            for tap_pos in tap_positions:
+                # update tap position in power grid input data
+                self.low_voltage_grid["transformer"]["tap_pos"] = [tap_pos]
+
+                # create power grid model with updated tap position
+                pow_grid_model = PowerGridModel(input_data=self.low_voltage_grid)
+
+                # run time series power flow calculation
+                pow_flow_result = pow_grid_model.calculate_power_flow(
+                    update_data=self.load_profile_batch, 
+                    calculation_method=CalculationMethod.newton_raphson,
+                    threading=0
+                )
+
+                # line losses
+                p_loss = pd.DataFrame()
+                p_loss = pd.DataFrame(pow_flow_result["line"]["p_from"]) + pd.DataFrame(pow_flow_result["line"]["p_to"])
+
+                table_line_losses = pd.DataFrame()
+                table_line_losses["energy_loss_kw"] = 0.0
+
+                for (column_name, column_data), i in zip(p_loss.items(), enumerate(p_loss.columns)):
+                    table_line_losses.loc[i[0], "energy_loss_kw"] = integrate.trapezoid(column_data.to_list()) / 1000
+
+                line_losses.append(sum(table_line_losses["energy_loss_kw"]))
+
             min_loss_idx = line_losses.index(min(line_losses))
             optimal_tap_pos = tap_positions[min_loss_idx]
+
         elif optimization_criteria == "minimize_voltage_deviations":
+            for tap_pos in tap_positions:
+                # update tap position in power grid input data
+                self.low_voltage_grid["transformer"]["tap_pos"] = [tap_pos]
+
+                # create power grid model with updated tap position
+                pow_grid_model = PowerGridModel(input_data=self.low_voltage_grid)
+
+                # run time series power flow calculation
+                pow_flow_result = pow_grid_model.calculate_power_flow(
+                    update_data=self.load_profile_batch, 
+                    calculation_method=CalculationMethod.newton_raphson,
+                    threading=0
+                )
+
+                # voltage deviations
+                table_voltages = pd.DataFrame()
+                table_voltages["max_pu"] = 0.0
+                table_voltages["min_pu"] = 0.0
+                i = 0
+                for node_scenario in pow_flow_result["node"]:
+                    df_temp = pd.DataFrame(node_scenario)
+                    max_value_pu = df_temp.at[df_temp["u_pu"].idxmax(), "u_pu"]
+                    min_value_pu = df_temp.at[df_temp["u_pu"].idxmin(), "u_pu"]
+                    table_voltages.loc[i, "max_pu"] = max_value_pu
+                    table_voltages.loc[i, "min_pu"] = min_value_pu
+                    i = i + 1
+
+                max_volt_deviation = max(max(abs(table_voltages["max_pu"] - 1)), max(abs(table_voltages["min_pu"] - 1)))
+                voltage_deviations.append(max_volt_deviation)
+
             min_volt_dev_idx = voltage_deviations.index(min(voltage_deviations))
             optimal_tap_pos = tap_positions[min_volt_dev_idx]
+        
         else:
             raise OptimalTapPositionCriteriaError("Criteria incorrect")
 
